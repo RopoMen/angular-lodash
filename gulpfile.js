88b3ba7bec9f0c97176cca13c0b61039d1ca53ad
Markku Roponen <roponen.markku@gmail.com>
2015-08-25 23:45:24 +0300

    Added build task with minification, this can be used when first Bower release is intended

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,22 @@
   'use strict';
 
   var gulp = require('gulp');
+  var dateFormat = require('dateformat');
+  var uglify = require('gulp-uglify');
+  var rename = require('gulp-rename');
+  var header = require('gulp-header');
   var KarmaServer = require('karma').Server;
   var jshint = require('gulp-jshint');
+  var pkg = {};
+  var banner = ['/**',
+    ' * <%= pkg.name %> - <%= pkg.description %>',
+    ' * @build time <%= buildTime %>',
+    ' * @author <%= pkg.author %>',
+    ' * @version v<%= pkg.version %>',
+    ' * @license <%= pkg.license %>',
+    ' */',
+    ''].join('\n');
+  var buildTime = '';
 
   gulp.task('jshint', function() {
     gulp.src([
@@ -15,10 +29,28 @@
   });
 
   gulp.task('test', ['jshint'], function() {
-	new KarmaServer({
+    new KarmaServer({
 	    configFile: __dirname + '/karma.config.js',
 	    singleRun: true
 	  }).start();
+  });
+
+  gulp.task('minify', function() {
+    pkg = require('./package.json');
+    buildTime = dateFormat(new Date(), 'yyyy-MM-dd HH:MM');
+
+    gulp.src('angular-lodash.js')
+      .pipe(uglify())
+      .pipe(header(banner, {pkg : pkg, buildTime: buildTime}))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./dist'));
+  });
+
+  gulp.task('build', ['test', 'minify'], function() {
+    /*Note: pkg and buildTime are resolved in minification process.*/
+    gulp.src('angular-lodash.js')
+      .pipe(header(banner, {pkg : pkg, buildTime: buildTime}))
+      .pipe(gulp.dest('./dist'));
   });
 
   gulp.task('default', ['jshint']);
